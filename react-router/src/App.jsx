c77c493196cd595f044c2d1e3ac8a846ac5e71c5
angelorapana <angelorapana@icloud.com>
2026-03-13 11:33:41 +0100

    Creata cartella Pages e fix del browseRouter

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
 
   return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/Chisiamo" element={<h1>Chi Siamo</h1>} />
+        <Route path="/Prodotti" element={<h1>Prodotti</h1>} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
