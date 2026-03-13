3ac2df961730d095ad113f0de36b5f7f5c8f6bdf
angelorapana <angelorapana@icloud.com>
2026-03-13 12:52:53 +0100

    Fix Pages Prodotti con creazione variabile con stato aggiornata da chiamata api e creazione card con metodo map

--- react-router/src/Pages/Prodotti.jsx
+++ react-router/src/Pages/Prodotti.jsx
@@ -1,8 +1,41 @@
-function Prodotti() {
+import { useState, useEffect, use } from "react";
+import axios from "axios";
 
+function Prodotti() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products")
+            .then((res) => setProducts(res.data))
+            .catch((error) => alert("Errore caricamento prodotti"));
+
+    }, []);
     return (
-        <h1 className="p-3">Prodotti</h1>
-    )
+
+        <div className="container mt-5">
+            <h1 className="mb-4 text-center">Prodotti</h1>
+            <div className="row">
+                {products.map((product) => (
+                    <div key={product.id} className="col-4 mb-4">
+                        <div className="card h-100 text-center p-3">
+                            <img src={product.image}
+                                className="card-img-top"
+                                alt={product.title}
+                                style={{ height: "200px", objectFit: "contain" }} />
+                            <div className="card-body">
+                                <h6 className="card-title">
+                                    {product.title}
+                                </h6>
+                                <p className="card-text fw-bold">
+                                    {product.price} €
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 
