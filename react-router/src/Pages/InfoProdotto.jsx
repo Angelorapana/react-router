31e52e20cd59541c9213ad326caf9cb8d7cdd56f
angelorapana <angelorapana@icloud.com>
2026-03-13 14:20:45 +0100

    Fix Pages InfoProdotto

--- react-router/src/Pages/InfoProdotto.jsx
+++ react-router/src/Pages/InfoProdotto.jsx
@@ -1,13 +1,33 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function DettaglioProdotto() {
     const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((res) => setProduct(res.data))
+            .catch((error) => alert("Errore caricamento informazioni prodotto"));
+    }, []);
 
     return (
-        <div>
-            <h1>Info di {id}</h1>
-        </div>
-    );
+        <>
+            <div className="container mt-5">
+                <h1 className="mb-4">{product?.title}</h1>
+                {product && (
+                    <div className="card p-4 text-center">
+                        <img
+                            src={product.image}
+                            alt={product.title}
+                            style={{ height: "250px", objectFit: "contain" }}
+                        />
+                        <h3 className="mt-3">{product.price} €</h3>
+                        <p className="mt-3">{product.description}</p>
+                    </div>)}
+            </div>
+        </>);
 }
 
 export default DettaglioProdotto;
